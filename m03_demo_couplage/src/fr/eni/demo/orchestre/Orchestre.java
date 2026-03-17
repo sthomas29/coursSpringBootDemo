0a44c617af180957a7815a2847d93eb2097c5976
Stéphane THOMAS <thomas91@free.fr>
2026-03-17 10:59:08 -0300

    Modification de l'orchestre (Liste de Musiciens) plus de nécessité de tester le type Pianiste ou Violoniste (plus de dépendances forte désormais)

--- m03_demo_couplage/src/fr/eni/demo/orchestre/Orchestre.java
+++ m03_demo_couplage/src/fr/eni/demo/orchestre/Orchestre.java
@@ -4,24 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Orchestre {
-    private List<Object> listeMusiciens;
+    private List<Musicien> listeMusiciens;
 
     public Orchestre() {
         listeMusiciens = new ArrayList<>();
     }
 
-    public void ajout(Object musicien) {
+    public void ajout(Musicien musicien) {
         listeMusiciens.add(musicien);
     }
 
     public void jouer() {
-        for(Object object: listeMusiciens) {
+        for(Musicien musicien: listeMusiciens) {
+
+            // Qu'importe l'instrument, le musicien peut jouer le morceau
+            musicien.jouerMorceau();
+
+            // Plus besoin de vérifier le type et de caster
+            /*
             if(object instanceof Pianiste) {
                 ((Pianiste) object).jouerMorceau();
             }
             if(object instanceof Violoniste) {
                 ((Violoniste) object).jouerMorceau();
             }
+            */
         }
     }
 }
