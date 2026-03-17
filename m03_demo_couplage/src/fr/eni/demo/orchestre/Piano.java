5bfea85357d3f7cb97b39f791bb33557c6cc6b90
Stéphane THOMAS <thomas91@free.fr>
2026-03-17 10:29:44 -0300

    Classe BO Piano et Violon

---
+++ m03_demo_couplage/src/fr/eni/demo/orchestre/Piano.java
@@ -0,0 +1,10 @@
+package fr.eni.demo.orchestre;
+
+public class Piano {
+    public void afficher() {
+        System.out.println("Je suis un piano...");
+    }
+    public void jouer() {
+        System.out.println("LA LA LA");
+    }
+}
