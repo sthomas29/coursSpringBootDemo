a3fd491f11a3b5be0397ad920d2f62965eb8e384
Stéphane THOMAS <thomas91@free.fr>
2026-03-17 11:04:30 -0300

    Modification de la classe d'exécution, l'orchestre est composé de musicien, il n'y a plus de dépendances directes avec les instruments Piano et Violon et les joueurs Pianiste et Violoniste

--- m03_demo_couplage/src/fr/eni/demo/orchestre/DemoApplication.java
+++ m03_demo_couplage/src/fr/eni/demo/orchestre/DemoApplication.java
@@ -3,11 +3,11 @@ package fr.eni.demo.orchestre;
 public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("Le pianiste : ");
-        Pianiste pianiste = new Pianiste("La 9eme de Beethoven");
+        Musicien pianiste = new Musicien("La 9eme de Beethoven", new Piano());
         pianiste.jouerMorceau();
 
         System.out.println("Le violoniste : ");
-        Violoniste violoniste = new Violoniste("La 9eme de Beethoven");
+        Musicien violoniste = new Musicien("La 9eme de Beethoven", new Violon());
         violoniste.jouerMorceau();
 
         System.out.println("L'orchestre : ");
