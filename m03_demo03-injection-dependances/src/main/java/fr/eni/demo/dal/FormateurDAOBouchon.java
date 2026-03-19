6cd405c4e843847bc606ed8d7211bcc1c0e1f056
Stéphane THOMAS <thomas91@free.fr>
2026-03-19 10:37:25 -0300

    Ajout FormateurDAOBouchon pour simuler la BDD

---
+++ m03_demo03-injection-dependances/src/main/java/fr/eni/demo/dal/FormateurDAOBouchon.java
@@ -0,0 +1,29 @@
+package fr.eni.demo.dal;
+
+import fr.eni.demo.bo.Formateur;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FormateurDAOBouchon implements FormateurDAO {
+
+    // Solution temporaire - gestion d'une liste de formateur locale
+    private static List<Formateur> lstFormateurs;
+
+    public FormateurDAOBouchon() {
+        lstFormateurs = new ArrayList<Formateur>();
+        lstFormateurs.add(new Formateur("Baille", "Anne-Lise", "abaille@campus-eni.fr"));
+        lstFormateurs.add(new Formateur("Gobin", "Stéphane", "sgobin@campus-eni.fr"));
+        // Ajout d’un formateur pour différencier les bouchons des couches DAL et BLL
+        lstFormateurs.add(new Formateur("Trillard", "Julien", "jtrillard@campus-eni.fr"));
+    }
+
+    @Override
+    public void create(Formateur formateur) {
+        lstFormateurs.add(formateur);
+    }
+    @Override
+    public List<Formateur> findAll() {
+        return lstFormateurs;
+    }
+}
