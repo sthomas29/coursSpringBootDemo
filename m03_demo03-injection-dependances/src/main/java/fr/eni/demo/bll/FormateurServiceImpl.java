024cfbafd8413917cb8279fd736cfd926ba22a11
Stéphane THOMAS <thomas91@free.fr>
2026-03-19 11:24:46 -0300

    Suppression de @Primary et ajout de @Profile(default) sur FormateurServiceImpl

--- m03_demo03-injection-dependances/src/main/java/fr/eni/demo/bll/FormateurServiceImpl.java
+++ m03_demo03-injection-dependances/src/main/java/fr/eni/demo/bll/FormateurServiceImpl.java
@@ -3,13 +3,14 @@ package fr.eni.demo.bll;
 import fr.eni.demo.bo.Formateur;
 import fr.eni.demo.dal.FormateurDAO;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 // Ajout @Primary pour imposer à Spring l'utilisation de ce Bean.
 @Service
-@Primary
+@Profile("default")
 public class FormateurServiceImpl implements FormateurService {
     private FormateurDAO formateurDAO;
 
