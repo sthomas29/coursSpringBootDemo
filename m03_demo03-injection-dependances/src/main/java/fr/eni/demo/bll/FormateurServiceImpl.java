f0f76944eef2e1e08fa0f2938dd6ebe5d6630f84
Stéphane THOMAS <thomas91@free.fr>
2026-03-19 10:38:15 -0300

    Ajout FormateurServiceImpl pour redigirer les requêtes métier vers la DAL

---
+++ m03_demo03-injection-dependances/src/main/java/fr/eni/demo/bll/FormateurServiceImpl.java
@@ -0,0 +1,25 @@
+package fr.eni.demo.bll;
+
+import fr.eni.demo.bo.Formateur;
+import fr.eni.demo.dal.FormateurDAO;
+
+import java.util.List;
+
+public class FormateurServiceImpl implements FormateurService {
+    private FormateurDAO formateurDAO;
+
+    public FormateurServiceImpl(FormateurDAO formateurDAO) {
+        this.formateurDAO = formateurDAO;
+    }
+
+    @Override
+    public void add(String nom, String prenom, String email) {
+        Formateur formateur = new Formateur(nom, prenom, email);
+        formateurDAO.create(formateur);
+    }
+    @Override
+    public List<Formateur> getFormateurs() {
+        System.out.println("From Implémentation");
+        return formateurDAO.findAll();
+    }
+}
