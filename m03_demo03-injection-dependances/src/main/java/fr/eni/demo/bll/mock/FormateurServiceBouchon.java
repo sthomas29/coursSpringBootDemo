4f45833b4359420b55aff1ab621c037989035857
Stéphane THOMAS <thomas91@free.fr>
2026-03-19 11:25:21 -0300

    Suppression de @Primary et ajout de @Profile(dev) sur mock/FormateurServiceBouchon

--- m03_demo03-injection-dependances/src/main/java/fr/eni/demo/bll/mock/FormateurServiceBouchon.java
+++ m03_demo03-injection-dependances/src/main/java/fr/eni/demo/bll/mock/FormateurServiceBouchon.java
@@ -3,12 +3,14 @@ package fr.eni.demo.bll.mock;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import fr.eni.demo.bll.FormateurService;
 import fr.eni.demo.bo.Formateur;
 
 @Service
+@Profile("dev")
 public class FormateurServiceBouchon implements FormateurService {
     // Solution temporaire - gestion d'une liste de formateur locale
     private static List<Formateur> lstFormateurs;
